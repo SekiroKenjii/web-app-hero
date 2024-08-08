14c1fb5b46659c2ff30d8955cc5b7db372a6b43f
Thuong Vo <trungthuongvo109@gmail.com>
2024-08-08 22:21:26 +0700

    implemented JWT service

--- src/WebAppHero.Application/Abstractions/IJwtService.cs
+++ src/WebAppHero.Application/Abstractions/IJwtService.cs
@@ -1,3 +1,12 @@
+using System.Security.Claims;
+
 namespace WebAppHero.Application.Abstractions;
 
-public interface IJwtService { }
+public interface IJwtService
+{
+    string? GenerateAccessToken(IEnumerable<Claim> claims);
+
+    string GenerateRefreshToken();
+
+    ClaimsPrincipal? GetClaimsPrincipalFormExpiredToken(string token);
+}
--- src/WebAppHero.Infrastructure/Authentication/JwtService.cs
+++ src/WebAppHero.Infrastructure/Authentication/JwtService.cs
@@ -1,5 +1,72 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using WebAppHero.Application.Abstractions;
+using WebAppHero.Infrastructure.DependencyInjection.Options;
 
 namespace WebAppHero.Infrastructure.Authentication;
 
-public class JwtService : IJwtService { }
+public class JwtService : IJwtService
+{
+    private readonly JwtOption jwtOption = new();
+
+    public JwtService(IConfiguration configuration)
+    {
+        configuration.GetSection(nameof(JwtOption)).Bind(jwtOption);
+
+        ArgumentNullException.ThrowIfNull(jwtOption.SecretKey);
+    }
+
+    public string? GenerateAccessToken(IEnumerable<Claim> claims)
+    {
+        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOption.SecretKey!));
+        var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        var jwtSecurityToken = new JwtSecurityToken(
+            issuer: jwtOption.Issuer,
+            audience: jwtOption.Audience,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(jwtOption.ExpireInMinutes),
+            signingCredentials: signinCredentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+    }
+
+    public string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+
+        return Convert.ToBase64String(randomNumber);
+    }
+
+    public ClaimsPrincipal? GetClaimsPrincipalFormExpiredToken(string token)
+    {
+        var key = Encoding.UTF8.GetBytes(jwtOption.SecretKey!);
+
+        var tokenValidationParameters = new TokenValidationParameters {
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ClockSkew = TimeSpan.Zero
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+
+        if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+            !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+        {
+            return default;
+        }
+
+        return principal;
+    }
+}
